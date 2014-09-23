3ca8cbc397a7c2ae21c09a83f1570e86a8a7980d
blehw <winton2095@gmail.com>
2014-09-23 19:41:08 -0400

    commited homework

---
+++ 05-codingbat/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[]args){
+	StringStuff s = new StringStuff();
+	System.out.println(s.nonStart("Hello", "There"));
+	System.out.println(s.makeAbba("Hi", "Bye"));
+	System.out.println(s.diff21(5));
+	    }
+}
---
+++ 05-codingbat/StringStuff.java
@@ -0,0 +1,18 @@
+public class StringStuff {
+
+public String nonStart(String a, String b) {
+  return a.substring(1) + b.substring(1);
+}
+
+public String makeAbba(String a, String b) {
+  return a + b + b +a;
+}
+
+public int diff21(int n) {
+  if (n > 21) {
+    return 2 * Math.abs(n-21);
+  } else {
+    return Math.abs(n-21);}
+}
+
+}
