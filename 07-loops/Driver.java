758c185ba158b94bee01efca163e60cb731e1a28
blehw <winton2095@gmail.com>
2014-10-02 01:51:30 -0400

    commited Shapes.java

--- 07-loops/Driver.java
+++ 07-loops/Driver.java
@@ -1,6 +1,9 @@
 public class Driver {
     public static void main(String[] args) {
-	Triangle t = new Triangle();
+	Shapes t = new Shapes();
+	System.out.println(t.box(4,5));
+	System.out.println(t.box(5,4));
 	System.out.println(t.triangle1(4));
+	System.out.println(t.triangle2(5));
     }
 }
