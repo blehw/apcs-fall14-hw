cb61d1e06aa642f77b02e1e903f5caeea34ea3ab
blehw <winton2095@gmail.com>
2014-10-03 00:05:07 -0400

    commited homework 7 pt 2

--- 07-loops/Driver.java
+++ 07-loops/Driver.java
@@ -3,7 +3,10 @@ public class Driver {
 	Shapes t = new Shapes();
 	System.out.println(t.box(4,5));
 	System.out.println(t.box(5,4));
-	System.out.println(t.triangle1(4));
-	System.out.println(t.triangle2(5));
+	System.out.println(t.tri1(4));
+	System.out.println(t.tri2(5));
+	System.out.println(t.tri3(4));
+	System.out.println(t.diamond(7));
+	System.out.println(t.tri4(5));
     }
 }
--- 07-loops/Shapes.java
+++ 07-loops/Shapes.java
@@ -14,7 +14,7 @@ public class Shapes {
 	}
 	return s;
     }
-    public String triangle1(int h) {
+    public String tri1(int h) {
 	String t = "";
 	int counter = 0;
 	int row = 1;
@@ -29,7 +29,7 @@ public class Shapes {
 	}
 	return t;
     }
-    public String triangle2(int h) {
+    public String tri2(int h) {
 	String t = "";
 	int counter = h;
 	int row = h;
@@ -48,5 +48,87 @@ public class Shapes {
 	}
 	return t;
     }	
+    public String tri3(int h) {
+	int row;
+	int stop = ((h * 2)-1)/ 2;;
+	int start = stop + 1;
+	int counter;
+	int lastline;
+	String s = "";
+	for (row = 0; row < h-1; row = row + 1) {
+	    for (counter=1;counter<=stop;counter=counter+1) {
+		s = s + " ";
+	    }
+	    for (counter=counter;counter<=start;counter=counter+1) {
+		s = s + "*";
+	    }
+	    for (counter=counter;counter<=h*2-1;counter=counter+1) {
+		s = s + " ";
+	    }
+	    stop = stop -1;
+	    start = start + 1;
+	    s = s + "\n" ;
+	}	    
+	for (lastline=1;lastline<=h*2-1;lastline=lastline+1) {
+	    s = s + "*";
+	}
+	s = s + "\n";
+	return s;
+    }
+    //not working
+    public String diamond(int h) {
+	int top = (h-1)/2;
+        int row;
+	int stop = ((top * 2)-1)/ 2;;
+	int start = stop + 1;
+	int counter;
+	int lastline;
+	int middle;
+	String s = "";
+	for (row = 0; row < top-1; row = row + 1) {
+	    s = s + " ";
+	    for (counter=1;counter<=stop;counter=counter+1) {
+		s = s + " ";
+	    }
+	    for (counter=counter;counter<=start;counter=counter+1) {
+		s = s + "*";
+	    }
+	    for (counter=counter;counter<=top*2-1;counter=counter+1) {
+		s = s + " ";
+	    }
+	    stop = stop -1;
+	    start = start + 1;
+	    s = s + "\n" ;
+	}	    
+	for (lastline=1;lastline<=top*2-1;lastline=lastline+1) {
+	    s = s + "*";
+	}
+	s = s + "\n";
+	for (middle=1;middle<=h;middle=middle+1) {
+	    s = s + "*";
+	}
+	s = s + "\n";
+	return s;
+    }
+    public String tri4(int h) {
+	String t = "";
+	int counter = h;
+	int row = h;
+	while (row > 0) {
+	    while (counter > row) {
+		t = t + " ";
+		counter = counter - 1;
+	    }
+	    while (counter > 0) {
+		t = t + "*";
+		counter = counter - 1;
+	    }
+	    t = t + "\n";
+	    row = row - 1;
+	    counter = h;
+	}
+	return t;
+    }
+
 }
 
