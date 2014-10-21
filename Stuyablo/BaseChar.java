804f1a5803d22b2d1d3387887e798f1a50700ef9
Winton Yee <winton2095@gmail.com>
2014-10-21 08:37:36 -0400

    more stuffs

--- Stuyablo/BaseChar.java
+++ Stuyablo/BaseChar.java
@@ -1,19 +1,21 @@
+import java.util.Random;
 public class BaseChar{
-    
-    private int hp,mp,atk,def,spd,acc,lck;
-    private bool poisonState=false;
-    private String name, weapon;
 
-    public BaseChar(int h, int m, int a, int d, int s, int ac, int c, String n){
-	hp= h;
+    private int hp,mp,atk,def,spd,acc,lck;
+    private boolean poisonState=false;
+    private String name, weapon;
+    Random r = new Random();
+
+    public BaseChar(int h, int m, int a, int d, int s, int ac, int c, String n, String w){
+	hp=h;
 	mp=m;
-	atk=a;
-	def=d;
-	spd=s;
-	acc=ac;
-	lck=c;
+	atk=r.nextInt(3)+a-1;
+	def=r.nextInt(4)+d-2;
+	spd=r.nextInt(2)+s-1;
+	acc=r.nextInt(10)+ac-5;
+	lck=r.nextInt(3)+c-1;
 	name=n;
-	//I'll add some degree of randomness later.
+	weapon=w;
     }
     public String toString(){
 	return name;
@@ -21,6 +23,12 @@ public class BaseChar{
     
     public int getHP(){
 	return hp;
+    }
+    public int getMP(){
+	return mp;
+    }   
+    public void setMP(int m){
+	mp=m;
     }
     public void setHP(int h){
         hp = h;
@@ -37,11 +45,14 @@ public class BaseChar{
     public int getLck(){
 	return lck;
     }
+    public int getACC{
+	return acc;
+    }
     public String getWeapon(){
 	return weapon;
     }
-    public bool isPoisoned(){
-	return poison;
+    public boolean isPoisoned(){
+	return poisonState;
     }
     public String poison(){
 	if (!poisonState){
@@ -51,11 +62,18 @@ public class BaseChar{
 	return "" + this + "was already poisoned.";
     }
     public String attack(BaseChar other){
-	if (other.getDef()>this.getAtk()){
-	    return "" + this + " could deal no damage to " + other;
+	if (r.nextInt(100)+1<acc){
+	    if (other.getDef()>this.getAtk()){
+		return "" + this + " could deal no damage to " + other;
+	    }
+	    if (r.nextInt(100)+1<lck){
+		other.setHP(other.getHP()+2*(other.getDef()-this.getAtk()));
+		return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
+	    }
+	    other.setHP(other.getHP()+other.getDef()-this.getAtk());
+	    return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
 	}
-	other.setHP(other.getHP()+other.getDef()-this.getAtk());
-	return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
+	return "" + this + " missed " + other;
     }
 
 }
--- Stuyablo/Driver.java
+++ Stuyablo/Driver.java
@@ -6,30 +6,54 @@ public class Driver {
 	Scanner in = new Scanner(System.in);
 	String name;
 	String enter; 
-
-	
+	int i; 
 
 	System.out.println("Welcome, young traveler! What is your name?");
 	name = in.nextLine();
-	System.out.println("Hello " + name + "! Enter the world of GENERIC RPG FANTASY LAND...");
+
+	Mage m = new Mage();
+	Monster e = new Monster();
+
+	System.out.print("Hello " + name + "! Enter the world of GENERIC RPG FANTASY LAND");
+	for (i=1;i<3;i=i+1) {
 	try{
-	    Thread.sleep(2000);
+	    Thread.sleep(1000);
 	} catch(Exception e){}
-	System.out.println("You wake up in a forest. Press enter to look around.");
-	enter = in.nextLine();
+	System.out.print(".");
+	}
         try{
 	    Thread.sleep(1000);
 	} catch(Exception e){}
-	System.out.print(".");
+	System.out.println(".");
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.print("You wake up in a forest. (Hint: The 'enter' button is your friend.)");
+	enter = in.nextLine();
+        for (i=1;i<4;i=i+1) {
 	try{
 	    Thread.sleep(1000);
 	} catch(Exception e){}
 	System.out.print(".");
+	}
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+	System.out.print("Well, there are a lot of trees. I mean, it's a forest.");
+
+	enter = in.nextLine();
+        for (i=1;i<4;i=i+1) {
 	try{
 	    Thread.sleep(1000);
 	} catch(Exception e){}
 	System.out.print(".");
-	System.out.println("Well, there are a lot of trees. I mean, it's a forest.");
+	}
+	try{
+	    Thread.sleep(1000);
+	} catch(Exception e){}
+
+	System.out.println("What?!?!?!?! A strange traveler in the woods?!?!?!?! FIGHT HIM?!?!?! Get to it, " + name + "!");
+        enter = in.nextLine();
     }
 }
 
