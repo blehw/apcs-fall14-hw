c378022376021c33bf7c62f0b834c6137d8543be
blehw <winton2095@gmail.com>
2014-10-21 21:24:50 -0400

    commited working versions

--- Stuyablo/Driver.java
+++ Stuyablo/Driver.java
@@ -12,8 +12,8 @@ public class Driver {
 	System.out.println("Welcome, young traveler! What is your name?");
 	name = in.nextLine();
 
-	Mage mage = new Mage(100, 100, 4, 45, 6, 7, 759, name, "staff");
-	Monster monster = new Monster(100, 100, 4543, 543, 764, 565, 3, "Evil Travelining Salesman", "misc. body parts");
+	Mage mage = new Mage(100, 100, 100, 100, 100, 100, 100, name, "staff");
+	Monster monster = new Monster(100, 100, 100, 100, 100, 100, 3, "Evil Travelining Salesman", "misc. body parts");
 
 	System.out.print("Hello " + name + "! Enter the world of GENERIC RPG FANTASY LAND");
 	for (i=1;i<3;i=i+1) {
@@ -55,11 +55,12 @@ public class Driver {
 
 	System.out.println("What?!?!?!?! A strange traveler in the woods?!?!?!?! FIGHT HIM?!?!?! Get to it, " + name + "!");
         enter = in.nextLine();
-	System.out.println("You and your opponent have 100 HP, but you are a mage?!?!?!? And have 100 mana?!?! Yeah! Type 'spell1' to cast a fireball that will do 10 damage and cost 20 mana (but may miss), spell2 to cast a lightning bolt that will do 10 damage and cost 30 mana (but never miss), and spell3 to cast a big ol' snowball that will do 20 damage and cost 40 mana (but may miss).");
+	System.out.print("You and your opponent have 100 HP, but you are a mage?!?!?!? And have 100 mana?!?! Yeah! Type 'spell1' to cast a fireball that will do 10 damage and cost 20 mana (but may miss), spell2 to cast a lightning bolt that will do 10 damage and cost 30 mana (but never miss), and spell3 to cast a big ol' snowball that will do 20 damage and cost 40 mana (but may miss).");
 	enter = in.nextLine();
 
 	while (mage.getHP() > 0 && monster.getHP() > 0) {
-	    
+	    spell = in.nextLine();
+	    System.out.println(mage.spell(monster, spell));
 	    
 	}
     }
