8f43646985d065b16d15f7e3d06439706be0969f
Winton Yee <wyee@github.com>
2014-10-28 08:04:22 -0400

    test

--- 13-arrays/Arrays.java
+++ 13-arrays/Arrays.java
@@ -1,29 +1,29 @@
 public class Arrays {
-public int[] frontPiece(int[] nums) {
-  int[] intArray;
-  if (nums.length<2) {
-    return nums;
-  } else {
-    intArray = new int[2];
-    for (int i=0;i<2;i=i+1) {
-      intArray[i]=nums[i];
+    public int[] frontPiece(int[] nums) {
+	int[] intArray;
+	if (nums.length<2) {
+	    return nums;
+	} else {
+	    intArray = new int[2];
+	    for (int i=0;i<2;i=i+1) {
+		intArray[i]=nums[i];
+	    }
+	    return intArray;
+	}
     }
-    return intArray;
-  }
-}
-public int sum13(int[] nums) {
-  int i=0;
-  if (nums.length<1) {
-    return 0;
-  } else {
-    for (int c=0;c<nums.length;c=c+1) {
-      if (nums[c]==13) {
-        c = c +1;
-      } else {
-        i = i + nums[c];
-      }
+    public int sum13(int[] nums) {
+	int i=0;
+	if (nums.length<1) {
+	    return 0;
+	} else {
+	    for (int c=0;c<nums.length;c=c+1) {
+		if (nums[c]==13) {
+		    c = c +1;
+		} else {
+		    i = i + nums[c];
+		}
+	    }
+	}
+	return i;
     }
-  }
-  return i;
-}
 }
