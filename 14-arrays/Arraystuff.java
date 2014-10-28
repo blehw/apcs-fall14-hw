6be9706662e0e92835fcf79225b0c933012765b3
blehw <winton2095@gmail.com>
2014-10-27 21:01:38 -0400

    commited homework 14

---
+++ 14-arrays/Arraystuff.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Arraystuff {
+    private int[] a;
+    private Random rnd; 
+
+    /* by making it final we can't change it after
+       the initial assignment
+    */
+    
+    public final int final_example = 123;
+
+    public Arraystuff(int n) {
+	rnd = new Random();
+	a = new int[n];
+	for (int i=0;i<a.length;i=i+1) {
+	    a[i] = 75 + rnd.nextInt(76);
+	}
+    }
+    
+    public Arraystuff() {
+	this(100);
+    }
+
+    public String toString() {
+	String s = "";
+	for (int i=0;i<a.length;i=i+1) {
+	    s = s + a[i]  + ", ";
+	}
+	return s;
+    }
+
+    /*
+    public String toString() {
+	String s = "" + a.length;
+	s = s + final_example;
+	//final_example = 321;
+	return s;
+    }
+    */
+
+    public int find(int value) {
+	for (int i=0;i<a.length;i=i+1) {
+	    if (a[i]==value) {
+		return i;
+	    } else {}
+	}
+	return -1;
+    }
+
+    public int maxVal() {
+	int max = a[0];
+	for (int i=0;i<a.length;i=i+1) {
+	    if (a[i]>max) {
+		max = a[i];
+	    } else {}
+	}
+	return max;
+    }
+
+}
---
+++ 14-arrays/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+	Arraystuff as = new Arraystuff();
+	System.out.println(as);
+	System.out.println(as.find(100));
+	System.out.println(as.maxVal());
+    }
+}
