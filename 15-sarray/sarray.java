6fc5a80def9d3ff29248f3ced9f043464d4ac849
blehw <winton2095@gmail.com>
2014-11-10 02:31:31 -0500

    commited final sarray

--- 15-sarray/sarray.java
+++ 15-sarray/sarray.java
@@ -34,35 +34,72 @@ public class sarray {
 	    last +=1;
 	}
     }
-
     
     public void add(int index, int i) {
-        
-    }
-    /*
-    public void get(int index) {
-	//get item at index
-    }
-
-    public void set(int index, int i) {
-	//set item at index to int i
+         if (last<index) {
+	     if (index<data.length) {
+		 data[index] = i;
+		 last = index;
+	     } else {
+		 while (data.length<index+1) {
+		     makeArray();
+		 }
+		 data[index] = i;
+		 last = index;
+	     }
+	 } else {
+	     makeArray();
+	     for (int k=last;k>=index;k=k-1) {
+		 data[k +1] = data[k];
+	     }
+	     data[index] = i; 
+	     last +=1;
+	 }
     }
 
     public int size() {
-	//return number of items in set
+	int counter = 0;			
+	for (int i=0;i<data.length;i++) {
+	    if (data[i]!=0) {
+		counter++;
+	    }
+	}
+	return counter;
+    }
+    
+    public int get(int index) {
+	return data[index];
     }
 
-    public void remove(int index) {
-	//removes item from set
+    public int set(int index, int i) {
+        int oldval = data[index];
+	data[index] = i;
+	return oldval;
+    }    
+
+    public int remove(int index) {
+	int oldval = data[index];
+	data[index] = 0;
+	return oldval;
     }
-    */
 
     public static void main(String[] args) {
 	sarray s = new sarray();
-	s.add(1);
+        s.add(1);
 	s.add(4);
 	System.out.println(s);
 	s.add(5);	
         System.out.println(s);
+	System.out.println("size: " + s.size());
+	s.add(2,3);
+	System.out.println(s);
+	s.add(11,5);
+	System.out.println(s);
+	System.out.println("index at 3: " + s.get(3));
+	System.out.println("size: " + s.size());
+	System.out.println(s.set(1,10));
+	System.out.println(s);
+	System.out.println(s.remove(0));
+	System.out.println(s);
     }
 }
