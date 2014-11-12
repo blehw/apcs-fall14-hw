2b5fb0f47f6a77f83dda779da3c0ca9c042b84c7
Winton Yee <wyee@github.com>
2014-11-12 08:41:26 -0500

    commited Driver

---
+++ 16-arrayList/Driver.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	ArrayList<Integer> al = new ArrayList<Integer>();
+	Random r = new Random();
+        for (int i = 0; i < 20; i++) {
+	    al.add(r.nextInt(5));
+	}
+	System.out.println(al);
+	for (int i = 0; i < al.size(); i++) {
+
+	}
+    }
+}
