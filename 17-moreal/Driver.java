5012e0848c4361b37e128cfbe4904b7ec1d0e70b
blehw <winton2095@gmail.com>
2014-11-16 11:04:42 -0500

    commited moreal

--- 17-moreal/Driver.java
+++ 17-moreal/Driver.java
@@ -29,10 +29,29 @@ public class Driver {
 	
 	for (int i=0;i<10;i++) {
 	    s = s + a[i] + ", ";
+	}	
+
+	int[]b = new int[10];
+	int x = 10;
+	int k = 0;
+
+	while (x>0) {
+	    int q = r.nextInt(x);
+	    b[q] = a[k];
+	    k = k + 1;
+	    x = x - 1;
+	    
+	}
+	    
+
+	String z = "";
+	
+	for (int i=0;i<10;i++) {
+	    z = z + b[i] + ", ";
 	}
 	
-
 	System.out.println(s);
+	System.out.println(z);
 	
     }
 }
