1adfa21edf4725f68e65d679bb5c3ee7518da74d
Winton Yee <wyee@github.com>
2014-12-01 08:38:17 -0500

    commited overwritten add(index,s) and attempt past first letter sorting

--- 19-osarray/osarray.java
+++ 19-osarray/osarray.java
@@ -10,20 +10,39 @@ public class osarray extends sarraystr {
 		if ((s.substring(0,1)).compareTo((super.get(i)).substring(0,1)) <= 0) {
 		    super.add(i,s);
 		    b = false;
-		} 
+		}
+		/*
+		if ((s.substring(0,1)).compareTo((super.get(i)).substring(0,1)) == 0) {
+		    int j = 1;
+		    while ((s.substring(j,j+1)).compareTo((super.get(i)).substring(j,j+1)) == 0) {
+			if (j > s.length()) {
+			    super.add(j,s);
+			} else if (j < super.get(i).length()) {
+			    super.add(j+1,s);
+			} else if ((s.substring(j,j+1)).compareTo((super.get(i)).substring(j,j+1)) < 0) {
+			    super.add(j,s);
+			    b = false;
+			}
+			j = j + 1;
+		    }
+		}
+		*/	    
 		i = i + 1;
 	    }
 	    if (b) {
-		super.add(0,s);
+		super.add(s);
 	    }
 	}
+    }
+
+    public void add(int index, String s) {
+	this.add(s);
     }
 
     public String set(int i, String s) {
 	String old = super.get(i);
 	super.remove(i);
 	this.add(s);
-	this.add(old);
 	return old;
     }
  
@@ -35,8 +54,14 @@ public class osarray extends sarraystr {
 	System.out.println(s);
 	s.add("a");
 	System.out.println(s);
+	s.add("demo");
+	System.out.println(s);
 	s.add("cat");
 	System.out.println(s);
+	s.add(0,"y");
+	System.out.println(s);
+	s.add("add");
+	System.out.println(s);	
 	s.set(1,"hello");
 	System.out.println(s);
 	
