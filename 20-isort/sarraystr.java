9b379294e3af975f8efbb12915066ae7940cf53b
Winton Yee <wyee@github.com>
2014-12-08 08:38:20 -0500

    commited count for comparisons and assignments

--- 20-isort/sarraystr.java
+++ 20-isort/sarraystr.java
@@ -1,6 +1,7 @@
 public class sarraystr {
     private String[] data;
     private int last;
+    private String letters="abcdefghijklmnopqrstuvwxyz";
 
     public sarraystr() {
 	data = new String[10];
@@ -90,35 +91,66 @@ public class sarraystr {
 	}
     }
 
+    public void fill() {
+	for (int i=0;i>=0;i--) {
+	    data[i] = letters.substring(26-i,26-i+1);
+	    last = last + 1;
+	}
+    }
+
+    /*
+      isort and ssort are doing comparisons and assignments
+      Arrays.sort(data); sorts the entire array
+      you can also sort only a part of the array
+     */
+
+    //n*(n-1)/2, n^2 algorithm
     public void isort() {
+	int cCount = 0;
+	int aCount = 0;
 	String val;
 	int k;
 	for (int i=0;i<last+1;i++) {
 	    val = data[i];
+	    aCount = aCount + 1;
 	    for (k=i;k>0&&val.compareTo(data[k-1])<=0;k--) {
+		cCount = cCount + 1;
 		data[k] = data[k-1];
+		aCount = aCount + 1;
 	    }
 	    data[k]=val;
+	    aCount = aCount + 1;
 	}
+	System.out.println("isort comparison count: " + cCount);
+	System.out.println("isort assignment count: " + aCount);
     }
 
+    //n^2 algorithm
     public void ssort() {
+	int cCount = 0;
+	int aCount = 0;
 	String min = "";
 	String oldstr="";
 	int index=0;
 	for (int i=0;i<=last;i++) {
 	    int k=i;
 	    min=data[k];
+	    aCount = aCount + 2;
 	    for (k=i;k<=last;k++) {
 		if (data[k].compareTo(min)<0) {
+		    cCount = cCount + 1;
 		    min = data[k];
 		    index=k;
+		    aCount = aCount + 2;
 		}
 	    }
 	    oldstr = data[i];
 	    data[i] = min;
 	    data[index] = oldstr;
+	    aCount = aCount + 3;
 	}
+	System.out.println("ssort comparison count: " + cCount);
+	System.out.println("ssort assignment count: " + aCount);
     }
 
     public boolean bcheck() {
@@ -145,20 +177,30 @@ public class sarraystr {
     
     public static void main(String[] args) {
 	sarraystr s1 = new sarraystr();
+	/*
         s1.add("c");
 	s1.add("z");
 	s1.add("b");	
 	s1.add("a");
 	s1.add("q");
+	6 comparisons, 16 assignments
+	s1.fill();
+	45 comparisons, 65 assignments
+	*/
 	System.out.println("before isort:\n"+ s1);
 	s1.isort();
 	System.out.println("after isort:\n"+s1);
 	sarraystr s2 = new sarraystr();
+	/*
 	s2.add("c");
 	s2.add("z");
 	s2.add("b");	
 	s2.add("a");
 	s2.add("q");
+	5 comparisons, 35 assignments
+	s1.fill();
+	25 comparisons, 100 assignments
+	*/
 	System.out.println("before ssort:\n"+ s2);
 	s2.ssort();
 	System.out.println("after ssort:\n"+s2);
