72512655ba0c3e40d60b80a33b0aafc36fb5b74f
blehw <winton2095@gmail.com>
2014-12-09 23:42:22 -0500

    commited compareTo

---
+++ 21-interval/Interval.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+class Interval {
+
+		private int low,high;
+		private Random r = new Random();				
+		private static int numIntervals = 0;
+		
+		public Interval(int l, int h){
+				low = l;
+				high = h;
+				numIntervals = numIntervals + 1;
+		}
+		
+		public Interval() {
+				int l = r.nextInt(100);
+				int h = l + 1 + r.nextInt(100);
+				low = l;
+				high = h;
+				numIntervals = numIntervals + 1;
+		}
+
+    public int getLow() {
+	return low;
+    }
+
+    public int getHigh() {
+	return high;
+    }
+		public String toString() {
+				//String s = "Inteval: "+numIntervals+": "+ "["+low+","+high+"]";
+				String s = "["+low+","+high+"]";
+
+				return s;
+		}
+
+    public int compareTo(Interval other) {
+	if (other.getLow() == low) {
+	    if (other.getHigh() == high) {
+		return 0;
+	    } else if (other.getHigh() < high) {
+		return 1;
+	    } else if (other.getHigh() > high) {
+		return -1;
+	    } else {
+		if (other.getLow() < low) {
+		    return 1;
+		} else {
+		    return -1;
+		}
+	    }
+	}
+	return 0;
+    }
+
+
+		public static void printstuff() {
+				System.out.println("Stuff");
+		}
+		public static void main(String[] args) {
+
+				Interval ival = new Interval();
+				ival.printstuff(); // normal calling off of an instance
+				printstuff(); // since printStuff is static we can call it without an instance
+				Interval.printstuff(); // we can also call it right off a class
+
+				Interval[] a = new Interval[10];
+				for (int i = 0; i < a.length; i++) {
+						a[i] = new Interval();
+				}
+				System.out.println(Arrays.toString(a));
+		}
+}
